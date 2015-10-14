6e8e651fc5279ae00c541f9009d4aab68188a72b
wuyanxin <yanxin.wu@qq.com>
2015-10-14 19:43:47 +0800

    add test/error

---
+++ test/error.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2015 Meizu bigertech, All rights reserved.
+ * http://www.bigertech.com/
+ * @author wuyanxin
+ * @date  15/10/14
+ * @description
+ *
+ */
+
+var assert = require('assert');
+
+describe('needAnError', function () {
+  it('iGiveYou', function () {
+    assert.equal(1, 2, '1 equals 2');
+  });
+});
