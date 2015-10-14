a20807594c762fd659590a68191d483f902b9420
wuyanxin <yanxin.wu@qq.com>
2015-10-14 19:44:07 +0800

    add test/success

---
+++ test/sucess.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2015 Meizu bigertech, All rights reserved.
+ * http://www.bigertech.com/
+ * @author wuyanxin
+ * @date  15/10/14
+ * @description
+ *
+ */
+
+var assert = require('assert');
+
+describe('needAnSuccess', function () {
+  it('iGiveYou', function () {
+    assert.equal(1, 1, '1 equals 1');
+  });
+});
